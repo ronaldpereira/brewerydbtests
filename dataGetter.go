e97fd3e9c992631a0a10813da0d972daaff357b4
Ronald Pereira <ronald.drp11@gmail.com>
2018-02-01 15:33:01 -0200

    Inserido alguns comentários no código

--- dataGetter.go
+++ dataGetter.go
@@ -9,15 +9,15 @@ import 	(
 		)
 
 func getBeers (url string) []byte{
-	resp, err := http.Get(url)
+	resp, err := http.Get(url) // Gets the response from the brewerydb API
 
-	defer resp.Body.Close()
+	defer resp.Body.Close() // Will close the response connection right after function returns
 
 	if err != nil {
 		panic(err)
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body) // Reads the response body
 
 	return body
 }
@@ -25,7 +25,7 @@ func getBeers (url string) []byte{
 func mapMount (respString []byte) string{
 	var m map[string]json.RawMessage
 
-	err := json.Unmarshal(respString, &m)
+	err := json.Unmarshal(respString, &m) // Converts a Json byte object to a map object
 
 	if err != nil {
 		panic(err)
@@ -35,7 +35,7 @@ func mapMount (respString []byte) string{
 }
 
 func dataSplitter (rawData string) []string {
-	splitData := strings.Split(rawData, ",\"")
+	splitData := strings.Split(rawData, ",\"") // Split the data string into separate fields by the ," marker
 
 	return splitData
 }
